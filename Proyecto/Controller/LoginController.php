89e3d438010d6994a8aff8b31ce2819c408320e6
JafethPM <poveda1390@gmail.com>
2025-02-06 05:41:07 -0600

    Commit final de clase

--- Proyecto/Controller/LoginController.php
+++ Proyecto/Controller/LoginController.php
@@ -1,24 +1,40 @@
 <?php
+    include_once $_SERVER["DOCUMENT_ROOT"] . "/Proyecto/Model/LoginModel.php";
 
-if(isset($_POST["btnIniciarSesion"]))
-{
-    //echo $_POST["txtIdentificacion"] . ' ' . $_POST["txtContrasenna"];
-    //Llamar al Modelo, pasarle la identificación y la contraseña
+    if(isset($_POST["btnRegistrarCuenta"]))
+    {
+        $identificacion = $_POST["txtIdentificacion"];
+        $nombre = $_POST["txtNombre"];
+        $correo = $_POST["txtCorreo"];
+        $contrasenna = $_POST["txtContrasenna"];
 
-    header('location: ../../View/Login/home.php');
-}
+        $resultado = RegistrarCuentaModel($identificacion,$nombre,$correo,$contrasenna);
 
-if(isset($_POST["btnRegistrarCuenta"]))
-{
-    /**/
-    $Identificacion=$_POST["txtIdentificacion"];
-    $nombre=$_POST["txtNombre"];
-    $correo=$_POST["txtCorreo"];
-    $contrasenna=$_POST["txtxontrasenna"];
-    
-    $resultado = btnRegistrarCuentaModel();
+        if($resultado == true)
+        {
+            header('location: ../../View/Login/login.php');
+        }
+        else
+        {
+            $_POST["Message"] = "Su información no fue registrada correctamente";
+        }
+    }
 
-    header('location: ../../View/Login/login.php');
-}
+    if(isset($_POST["btnIniciarSesion"]))
+    {
+        $identificacion = $_POST["txtIdentificacion"];
+        $contrasenna = $_POST["txtContrasenna"];
+
+        $resultado = IniciarSesionModel($identificacion,$contrasenna);
+
+        if($resultado != null && $resultado -> num_rows > 0)
+        {
+            header('location: ../../View/Login/home.php');
+        }
+        else
+        {
+            $_POST["Message"] = "Su información no fue validada correctamente";
+        }
+    }
 
 ?>
