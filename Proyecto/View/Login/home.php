3ce5e167daac4ad5eb7528ed00bc9d9ba31f28fa
JafethPM <poveda1390@gmail.com>
2025-03-05 18:59:40 -0600

    Update home.php

--- Proyecto/View/Login/home.php
+++ Proyecto/View/Login/home.php
@@ -1,5 +1,5 @@
 <?php
-    include_once $_SERVER["DOCUMENT_ROOT"] . "/ProyectoMN/Proyecto/Controller/LoginController.php"";
+    include_once $_SERVER["DOCUMENT_ROOT"] . "/ProyectoMN/Proyecto/Controller/LoginController.php";
     include_once $_SERVER["DOCUMENT_ROOT"] . "/ProyectoMN/Proyecto/View/layoutInterno.php";
     ?>
 
